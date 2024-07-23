831376a26ac989e41fafced41948d1d7031a2e57
InshutiSouvede <isouvede2@gmail.com>
2024-07-23 20:08:25 +0200

    rough UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { nanoid } from 'nanoid'
 import './App.css'
+import Button from './components/Button'
 
 function App() {
+  const numArr = [7,8,9,4,5,6,1,2,3]
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div className='w-max py-20 m-auto'>
+    <input type='text' className="h-10 bg-slate-400 w-full"  value={0} />
+    <div className="gap-0 bg-red-200 grid grid-cols-[75%_25%]">
+      <div className="gray grid grid-cols-3">
+      <Button isOperator={false} value ="AC"/>
+      <Button isOperator={false} value ="+/-"/>
+      <Button isOperator={false} value ="%"/>      
+      {numArr.map((el)=><Button key={nanoid()} isOperator={false} value={el} />)}
+      <Button span = {"col-span-2"} value ={0}/>
+      <Button isOperator={false} value ="."/>
+      </div>
+      <div className='flex flex-col'>
+      <Button isOperator = {true} value ="AC"/>
+      <Button isOperator = {true} value ="AC"/>
+      <Button isOperator = {true} value ="AC"/>
+      <Button isOperator = {true} value ="AC"/>
+      <Button isOperator = {true} value ="AC"/>
+      </div>
+    </div>
+      
+    </div>
   )
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button(props){
     return (
-        <>
-        <button>{props.value}</button>
-        </>
+        <div className={props.isOperator?`bg-orange-400 ${props.span?props.span:''}`:"bg-gray-200"}>
+        <button className=" w-20 h-24 border border-black">{props.value}</button>
+        </div>
     )
 }
