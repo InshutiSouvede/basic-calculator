7fb31d776bede4285f6ac38c8171e3da35508baa
InshutiSouvede <isouvede2@gmail.com>
2024-07-26 11:02:04 +0200

    Clean codes

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,18 @@ import Operator from './components/Operator'
 
 function App() {
   const [resultField, setResultField] = useState("0")
-  const [value1, setValue1] = useState(0)
   const [resultGiven, setResultGiven] = useState(false)
   const [op, setOp] = useState('')
   const [currentExpression, setCurrentExpression] = useState(0)
-  const [prevExpression, setPrevExpression] = useState(0)
   const numArr = [7, 8, 9, 4, 5, 6, 1, 2, 3], basicOperator = ['÷', 'x', '-', '+', '=']
 
   const numbers = numArr.map((el) => <Number handleClick={holdNumber} key={nanoid()} number={el} />)
   const operators = basicOperator.map((el) => <Operator handleOperation={holdOperator} key={nanoid()} bgColor="bg-orange-400" color="text-white" label={el} />)
+  
   function holdNumber(event) {
     const num = event.target.textContent
     if(resultGiven){
-      console.log("Number clicked right after equal")
-      setPrevExpression(0)
       setCurrentExpression(0)
-    }else{
-      console.log("Number clicked after some other value")
     }
     
     setResultField(prev => {      
@@ -36,6 +31,7 @@ function App() {
     })
     setResultGiven(false)
   }
+  
   function computeCurrentExpression(v1,operator,value2){
     if (operator == '+') {
       return parseFloat((parseFloat(v1) + +value2).toFixed(7))
@@ -54,12 +50,12 @@ function App() {
     }
     return value2
   }
+  
   function holdOperator(event) {
     const operator = event.target.textContent
 
     if (operator == 'AC') {
       if(resultGiven){
-        setPrevExpression(0)
         setCurrentExpression(0)
       }
       setResultField('0')
@@ -68,31 +64,24 @@ function App() {
       setResultField(prev => (+prev) * -1)
     }
     else if (operator === '=') {
-      let computedValue;
-      console.log("In equal, previous expression",prevExpression,"current expression",currentExpression,"current operation ",op," current value in result field",resultField)
       const newExpression = computeCurrentExpression(currentExpression,op,resultField)
-      console.log("previous expression is going to be",currentExpression,"current expression is going to be",newExpression)
       setResultGiven(true)
       setResultField(newExpression)
     }
     else {
       let newExpression;
+
       setResultGiven(false)
       setOp(operator)
-      console.log("previous expression",prevExpression,"current expression",currentExpression,"current operation ",op," current value in result field",resultField)
+
       if(resultGiven){
         newExpression = computeCurrentExpression(0,'',resultField)
       }else{
-
        newExpression = computeCurrentExpression(currentExpression,op,resultField)
       }
 
-      console.log("previous expression is going to be",currentExpression,"current expression is going to be",newExpression)
-      // setValue1(resultField)
       setResultField('0')
       setCurrentExpression(newExpression)
-      setPrevExpression(currentExpression)
-      setValue1(resultField)
     }
 
   }
